dbb8ba9ca4ff1f662a47e4cfba05143936a2e098
Zachary Aten <z_man@Zacharys-MacBook-Pro.local>
2017-10-21 07:05:53 -0400

    adding jade

--- express_website/app.js
+++ express_website/app.js
@@ -5,12 +5,15 @@ var nodemailer = require('nodemailer');
 
 var app = express();
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade')
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', function(req, res){
-  console.log("Hello World");
-})
+  res.render('index');
+});
 
 app.listen(3000);
 
