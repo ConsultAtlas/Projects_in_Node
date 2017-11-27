c5ec7e4536750fa052619d93c335532bb1101ab1
Zachary Aten <z_man@Zacharys-MacBook-Pro.local>
2017-11-27 16:10:11 -0500

    working on nodeauth app

--- express_website/app.js
+++ express_website/app.js
@@ -25,6 +25,33 @@ app.get('/contact', function(req, res){
   res.render('contact');
 });
 
+app.post('/contact/send', function(req, res){
+  var transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: 'zach.ateno@gmail.com',
+      password: ''
+    }
+  });
+  var mailOptions = {
+    from: "Brad Traversy <zach.aten@gmail.com>",
+    to: "zach.aten@maiarconsulting.com",
+    subject: 'Website Submission',
+    test: 'You have a submission with the following details... Name: ' +req.body.name+' Email: '+req.body.email+' Message: '+req.body.message,
+    html: '<p>You have a submission with the following details...<ul><li>Name: '+req.body.name+'</li><li>Email: '+req.body.email+'</li><li>Message: '+req.body.message+'</li></ul></p>'
+  };
+
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log('Error');
+      res.redirect('/');
+    } else {
+      console.log('Message Sent '+info.response);
+      res.redirect('/');
+    }
+  })
+});
+
 app.listen(3000);
 
 console.log("Server is running on port 3000...");
