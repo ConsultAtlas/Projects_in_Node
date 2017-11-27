6c0e57827828c3ad7053070853ca6bcda55e7c7c
Zachary Aten <z_man@Zacharys-MacBook-Pro.local>
2017-11-27 16:48:54 -0500

    added views for registration and login

--- nodeauth/routes/index.js
+++ nodeauth/routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+var getBootstrapNode = require('bootstrap-node');
+var bootstrapNode = getBootstrapNode();
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Members' });
 });
 
 module.exports = router;
